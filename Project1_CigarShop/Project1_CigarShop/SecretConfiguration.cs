3498317eff6320b8dffdc57a97f5f7ef6170272f
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-05-12 11:58:15 -0500

    initial project 1 upload

---
+++ Project1_CigarShop/Project1_CigarShop/SecretConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CigarShop.ConsoleUI
+{
+    internal static class SecretConfiguration
+    {
+        internal static readonly string ConnectionString = "Server=tcp:llewellynsql.database.windows.net,1433;Initial Catalog=Project0;Persist Security Info=False;User ID=thomas;Password=P@ssword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
